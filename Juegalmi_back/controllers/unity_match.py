cff30f5b4f22b3e7632ffe754929959e620b63b3
El dioss Abhinaya <dahalabhinaya126@gmail.com>
2025-02-10 10:04:38 +0100

    dsad

--- Juegalmi_back/controllers/unity_match.py
+++ Juegalmi_back/controllers/unity_match.py
@@ -4,14 +4,14 @@ import json
 
 class GameAPIController(http.Controller):
 
-    @http.route('/api/match/stats', type='json', auth='public', methods=['POST'])
+@http.route('/api/match/stats', type='json', auth='user', methods=['POST'])
     def record_match_stats(self, **kwargs):
         data = json.loads(request.httprequest.data)
         
         match_id = data.get('match_id')
         stats = data.get('player_stats', [])
 
-        match = request.env['game.match'].browse(match_id)
+        match = request.env['game.match'].sudo().browse(match_id)
         if not match:
             return {'status': 'error', 'message': 'Match not found.'}
 
