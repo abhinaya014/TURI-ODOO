c618458eb4fe36bbe34d97b6d26c83d88e447be2
abhinaya014 <abhinayadahal407@gmail.com>
2025-02-13 17:32:33 +0100

    dfdf

--- Juegalmi_back/__manifest__.py
+++ Juegalmi_back/__manifest__.py
@@ -11,7 +11,7 @@
         'views/match.xml',
         'views/menu.xml',
         'views/transic.xml',
-        'data/game_skin.xml',
+        'data/game_skins.xml',
     ],
     'installable': True,
     'application': True,
